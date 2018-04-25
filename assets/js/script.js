61955155b3ea37e70a57ea21de08fdebaea7093b
halbeisent <dev@halbeisen.fr>
2018-04-25 15:18:26 +0200

    Mise à jour des cartes et des scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
+// Création du module AngularJS shoesAPP
+var app = angular.module('shoesApp', []);
+// Définition du controleur shoesCtrl
+app.controller('shoesCtrl', function($scope, $http) {
+  // On récupère le json
+  $http.get('assets/json/products.json')
+  // et on remplit le scope.products
+  .then(function(model) {
+    $scope.products = model.data
+  });
+});
 
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
