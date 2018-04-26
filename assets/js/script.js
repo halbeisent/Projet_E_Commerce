4db838aab0bfa2bd9189e72c35116cdb12a31416
halbeisent <dev@halbeisen.fr>
2018-04-26 09:25:38 +0200

    Routage en place et fonctionnel sur la vue principale

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 // Création du module AngularJS shoesAPP
-var app = angular.module('shoesApp', []);
+var app = angular.module('shoesApp', ['ngRoute']);
+app.config(['$routeProvider', function($routeProvider) {
+  //Je configure mon routeProvider pour que la racine (/) affiche mon fomulaire
+  $routeProvider.when('/', {templateUrl : 'views/allProducts.html', controller: 'shoesCtrl'})
+  //Je le configure ensuite pour que ma vue de mail (/mailView) affiche les détails du message
+                .when('/:model', {templateUrl: 'views/byBrand.html', controller: 'shoesCtrl'})
+  }]);
 // Définition du controleur shoesCtrl
 app.controller('shoesCtrl', function($scope, $http) {
   // On récupère le json
