b10f9f4e0a25ea21215a01e0395e2d51dd1dd3f6
Anousone MOUNIVONGS <anousone.mounivongs@gmail.com>
2018-04-27 09:53:19 +0200

    Ajout de commentaires fichier .js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Création du module AngularJS shoesAPP
 var app = angular.module('shoesApp', []);
-// Définition du controleur shoesCtrl
+// Définition du controleur shoesCtrl avec module $scope et $http
 app.controller('shoesCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.catFilter = '';
   // On récupère le json
@@ -12,39 +12,41 @@ app.controller('shoesCtrl', ['$scope', '$http', function($scope, $http) {
 
 // Gestion CART _________________________________________________________________________________________________________
 
-  // Création du tableau cart vide
+  // Création du tableau cart
   $scope.cart = [];
-  // Création de la fonction findItemById avec la library underscore.js pour défnir les occurences via id
+  // Création de la fonction findItemById avec la library underscore.js pour défnir les occurences : True ou False
   var findItemById = function(items, id) {
     return _.find(items, function(model) {
       return model.prodRef === id;
     });
   };
-  // calcul du prix total en fonction de la quantité
-  $scope.getCost = function(model) {
-    return model.qtyCart * model.prodPrice;
-  };
-  
+
   // Utilisation de la fonction findItemById pour définir si présent dans le tableau, si présent = Qty + 1, sinon on push les donnée dans cart.
   $scope.addItem = function(itemToAdd) {
     var found = findItemById($scope.cart, itemToAdd.prodRef);
     if (found) {
-      found.qtyCart += itemToAdd.qtyCart;
+      found.qtyCart += itemToAdd.qtyCart; // si true = just +1
     }
     else {
-      $scope.cart.push(angular.copy(itemToAdd));}
+      $scope.cart.push(angular.copy(itemToAdd));} // si false on push les données dans le tableau carts
   };
-  // Calcul du total d'article dans tableau Cart
+
+  // calcul du prix total en fonction de la quantité
+  $scope.getCost = function(model) {
+    return model.qtyCart * model.prodPrice;
+  };
+
+  // Calcul du nombre total d'articles dans tableau Cart avec fonction reduc + sum
   $scope.totalQty = function() {
-    var total =  _.reduce($scope.cart, function(sum, model) {
+    var total =  _.reduce($scope.cart, function(sum, model) { // sum en tant qu'accu
       return sum + model.qtyCart;
     }, 0);
       return total;
   };
 
-  // Calcul du prix total via le total $scope.getCost dans tableau Cart
+  // Calcul du prix total via le total $scope.getCost dans tableau Cart avec fonction reduc + sum
   $scope.getTotal = function() {
-    var total =  _.reduce($scope.cart, function(sum, model) {
+    var total =  _.reduce($scope.cart, function(sum, model) { // sum en tant qu'accu
       return sum + $scope.getCost(model);
     }, 0);
       return total;
@@ -61,9 +63,5 @@ app.controller('shoesCtrl', ['$scope', '$http', function($scope, $http) {
 }]);// fin du controller
 
 $(function (){ // Chargement du script quand le doc est chargé
-  $('[data-toggle="popover"]').popover()
+  $('[data-toggle="popover"]').popover() // Fonction popover pour les Description en mode popover
 })
-
-$('.nav-link').on('click',function() {
-  $('.navbar-collapse').collapse('hide');
-});
